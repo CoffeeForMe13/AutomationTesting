51ce425b9dcbf2f93f40ee31997dfcd558f8babe
MarinAdrian <adymarin013@gmail.com>
2024-06-25 18:55:14 +0300

    extentReport code added to the project. The code generates an exception

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,4 +1,8 @@
 import Actions.SimpleActions;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utile.BaseTest;
@@ -28,5 +32,34 @@ public class SimpleTest extends BaseTest {
 
         System.out.println(loginText);
         System.out.println(expectedText);
+    }
+
+    @Test
+    public void extentTest(){
+        ExtentReports extent = new ExtentReports();
+        ExtentSparkReporter spark = new ExtentSparkReporter("extentReportDEMO.html");
+        spark.config().setTheme(Theme.DARK);
+        spark.config().setDocumentTitle("Automation Report");
+        spark.config().setReportName("Extent Reports Demo");
+        extent.attachReporter(spark);
+
+
+        ExtentTest test = extent.createTest("Login Test");   //Create a test node in the report
+        test.assignAuthor("Adrian").assignCategory("DEMO").assignCategory("Test").assignDevice("chrome 126");
+        test.pass("Login Test started successfully");       //Create a test step node in the report
+        test.info("URL is loaded");
+        test.info("Value entered");
+        test.pass("Login Test completed successfully");
+            //Removed debug, fatal, error --> To match junit of testng
+
+
+        ExtentTest test1 = extent.createTest("HomePage Test").assignAuthor("Adrian");   //Create a test node in the report
+        test1.pass("HomePage Test started successfully");       //Create a test step node in the report
+        test1.info("URL is loaded");
+        test1.info("Value entered");
+        test1.fail("HomePage Test completed miserably");
+
+        extent.flush(); //Unless you call this method, your report will not be written with logs
+
     }
 }

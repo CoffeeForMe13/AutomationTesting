22aabcf5cd428bb2c783a2d3ad11ae540baafb86
MarinAdrian <adymarin013@gmail.com>
2024-07-03 17:29:22 +0300

    more interactions with the Overview page were added. Account information is now extracted

--- homeAssignment/main/java/actions/OverviewPageActions.java
+++ homeAssignment/main/java/actions/OverviewPageActions.java
@@ -1,22 +1,68 @@
 package actions;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import webElements.OverviewPageElements;
 
-public class OverviewPageActions {
+import java.time.Duration;
+import java.util.stream.Collectors;
+
+public class OverviewPageActions extends MenuLinksActions{
 
     private final OverviewPageElements elements;
+    private Wait<WebDriver> wait;
 
     public OverviewPageActions(WebDriver driver){
+        super(driver);
         this.elements = new OverviewPageElements(driver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
 
     public String getWelcomeMessage(){
         return elements.welcomeMessage().getText();
     }
 
-    public void clickLogOutLink(){
-        elements.LogOutLink().click();
+//    public void clickLogOutLink(){
+//        elements.logOutLink().click();
+//    }
+//
+//    public void clickTransferFundsLink(){
+//        elements.transferFundsLink().click();
+//    }
+
+    public int getNoOfAccounts(){
+        return elements.noOfAccounts().size() - 1;
     }
+
+    public String getAccount1ID(){
+        wait.until(d -> elements.accountNo1().isDisplayed());
+        return elements.accountNo1().getText();
+    }
+
+    public String getAccount2ID(){
+        return elements.accountNo2().getText();
+    }
+
+    public String getAccount1Balance(){
+        wait.until(d -> elements.accountNo1Balance().isDisplayed());
+        return elements.accountNo1Balance().getText();
+    }
+
+    public String getAccount2Balance(){
+        wait.until(d -> elements.accountNo2Balance().isDisplayed());
+        return elements.accountNo2Balance().getText();
+    }
+
+//    public void clickOpenNewAccountLink(){
+//        elements.openNewAccountLink().click();
+//    }
+
+    public boolean checkAccount(String newAccount){
+        wait.until(d -> elements.accountNo1().isDisplayed());
+        return elements.accountIDWEList().stream().map(WebElement::getText).collect(Collectors.toList()).contains(newAccount);
+    }
+
 
 }
--- homeAssignment/main/java/webElements/OverviewPageElements.java
+++ homeAssignment/main/java/webElements/OverviewPageElements.java
@@ -4,11 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class OverviewPageElements {
+import java.util.List;
+
+public class OverviewPageElements extends MenuLinks {
 
     private final WebDriver driver;
 
     public OverviewPageElements(WebDriver driver){
+        super(driver);
         this.driver = driver;
     }
 
@@ -16,7 +19,39 @@ public class OverviewPageElements {
         return driver.findElement(By.cssSelector("#leftPanel > p"));
     }
 
-    public WebElement LogOutLink(){
-        return driver.findElement(By.cssSelector("a[href^='logout.htm']"));
+//    public WebElement logOutLink(){
+//        return driver.findElement(By.cssSelector("a[href^='logout.htm']"));
+//    }
+//
+//    public WebElement transferFundsLink(){
+//        return driver.findElement(By.cssSelector("a[href^='transfer.htm']"));
+//    }
+
+    public List<WebElement> noOfAccounts(){
+        return driver.findElements(By.cssSelector("#accountTable > tbody > tr"));
+    }
+
+    public WebElement accountNo1(){
+        return driver.findElement(By.cssSelector("#accountTable > tbody > tr:nth-child(1) > td:nth-child(1) > a"));
+    }
+
+    public WebElement accountNo2(){
+        return driver.findElement(By.cssSelector("#accountTable > tbody > tr:nth-child(2) > td:nth-child(1) > a"));
+    }
+
+    public List<WebElement> accountIDWEList(){
+        return driver.findElements(By.cssSelector("#accountTable > tbody > tr > td:nth-child(1) > a"));
+    }
+
+    public WebElement accountNo1Balance(){
+        return driver.findElement(By.cssSelector("#accountTable > tbody > tr:nth-child(1) > td:nth-child(2)"));
+    }
+
+    public WebElement accountNo2Balance(){
+        return driver.findElement(By.cssSelector("#accountTable > tbody > tr:nth-child(2) > td:nth-child(2)"));
+    }
+
+    public WebElement openNewAccountLink(){
+        return driver.findElement(By.cssSelector("a[href^='openaccount.htm']"));
     }
 }

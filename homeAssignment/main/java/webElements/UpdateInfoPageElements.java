29a3ed717d9610be37a1d2d0fab8657ec09e7d3f
MarinAdrian <adymarin013@gmail.com>
2024-07-09 13:28:04 +0300

    test implementation for personal info update

---
+++ homeAssignment/main/java/webElements/UpdateInfoPageElements.java
@@ -0,0 +1,53 @@
+package webElements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class UpdateInfoPageElements extends MenuLinks {
+
+    private final WebDriver driver;
+
+    public UpdateInfoPageElements(WebDriver driver) {
+        super(driver);
+        this.driver = driver;
+    }
+
+    public WebElement firstNameField(){
+        return driver.findElement(By.cssSelector("#customer\\.firstName"));
+    }
+
+    public WebElement lastNameField(){
+        return driver.findElement(By.cssSelector("#customer\\.lastName"));
+    }
+
+    public WebElement addressField(){
+        return driver.findElement(By.cssSelector("#customer\\.address\\.street"));
+    }
+
+    public WebElement cityField(){
+        return driver.findElement(By.cssSelector("#customer\\.address\\.city"));
+    }
+
+    public WebElement stateField(){
+        return driver.findElement(By.cssSelector("#customer\\.address\\.state"));
+    }
+
+    public WebElement zipCodeField(){
+        return driver.findElement(By.cssSelector("#customer\\.address\\.zipCode"));
+    }
+
+    public WebElement phoneNumberField(){
+        return driver.findElement(By.cssSelector("#customer\\.phoneNumber"));
+    }
+
+    public WebElement updateProfileButton(){
+        return driver.findElement(By.cssSelector("input[type=button]"));
+    }
+
+    public WebElement profileUpdateMessage(){
+        return driver.findElement(By.cssSelector("#updateProfileResult > .title"));
+    }
+
+
+}

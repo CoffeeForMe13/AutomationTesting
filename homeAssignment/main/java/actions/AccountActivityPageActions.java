0c7e88b02cb79747e9aab355eee5339011f25203
MarinAdrian <adymarin013@gmail.com>
2024-07-09 12:26:01 +0300

    update the test

--- homeAssignment/main/java/actions/AccountActivityPageActions.java
+++ homeAssignment/main/java/actions/AccountActivityPageActions.java
@@ -1,7 +1,11 @@
 package actions;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import webElements.AccountActivityPageElements;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class AccountActivityPageActions extends MenuLinksActions {
 
@@ -10,5 +14,30 @@ public class AccountActivityPageActions extends MenuLinksActions {
     public AccountActivityPageActions(WebDriver driver) {
         super(driver);
         this.elements = new AccountActivityPageElements(driver);
+    }
+
+    public String getAccountID(){
+        wait.until(d -> elements.transactionHistoryTableElement().isDisplayed());
+        wait.until(d -> elements.accountNumber().isDisplayed());
+        return elements.accountNumber().getText();
+    }
+
+    public String getAccountType(){
+        wait.until(d -> elements.accountType().isDisplayed());
+        return elements.accountType().getText();
+    }
+
+    public String getAccountBalance(){
+        wait.until(d -> elements.accountBalance().isDisplayed());
+        return elements.accountBalance().getText();
+    }
+
+    public String getAccountAvailableBalance(){
+        wait.until(d -> elements.accountAvailableBalance().isDisplayed());
+        return elements.accountAvailableBalance().getText();
+    }
+
+    public List<String> getTableRowsToList(){
+        return elements.transactionHistoryTableRows().stream().map(WebElement::getText).collect(Collectors.toList());
     }
 }
--- homeAssignment/main/java/webElements/AccountActivityPageElements.java
+++ homeAssignment/main/java/webElements/AccountActivityPageElements.java
@@ -1,6 +1,10 @@
 package webElements;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class AccountActivityPageElements extends MenuLinks {
 
@@ -10,5 +14,31 @@ public class AccountActivityPageElements extends MenuLinks {
         super(driver);
         this.driver = driver;
     }
+
+    public WebElement accountNumber(){
+        return driver.findElement(By.cssSelector("#accountId"));
+    }
+
+    public WebElement accountType(){
+        return driver.findElement(By.cssSelector("#accountType"));
+    }
+
+    public WebElement accountBalance(){
+        return driver.findElement(By.cssSelector("#balance"));
+    }
+
+    public WebElement accountAvailableBalance(){
+        return driver.findElement(By.cssSelector("#availableBalance"));
+    }
+
+    public List<WebElement> transactionHistoryTableRows(){
+        return driver.findElements(By.cssSelector("#transactionTable tr"));
+    }
+
+    public WebElement transactionHistoryTableElement(){
+        return driver.findElement(By.cssSelector("#transactionTable tr td"));
+    }
+
+
 
 }

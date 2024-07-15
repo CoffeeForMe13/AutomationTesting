d95df7eb969e5ed923e0c74ae3b72b6a470a4da8
MarinAdrian <adymarin013@gmail.com>
2024-07-15 13:24:17 +0300

    transfer funds made into a function

--- homeAssignment/test/java/tests/TransferFundsTest.java
+++ homeAssignment/test/java/tests/TransferFundsTest.java
@@ -3,6 +3,7 @@ package tests;
 import actions.OpenNewAccountPageActions;
 import actions.OverviewPageActions;
 import actions.TransferFundsPageActions;
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utilities.BaseTestFunctionality;
@@ -11,6 +12,7 @@ import utilities.ConfigurationLoader;
 import java.util.List;
 
 import static tests.LoginTest.login;
+import static tests.OpenNewAccountTest.getNewAccount;
 
 public class TransferFundsTest extends BaseTestFunctionality {
 
@@ -21,8 +23,7 @@ public class TransferFundsTest extends BaseTestFunctionality {
         initTest("Transfer founds between accounts");
 
         //Make initialization
-        OverviewPageActions overviewPage = new OverviewPageActions(driver);
-        TransferFundsPageActions transferFundsPage = new TransferFundsPageActions(driver);
+//        overviewPage = new OverviewPageActions(driver);
         OpenNewAccountPageActions openNewAccountPage = new OpenNewAccountPageActions(driver);
 
         //Get registration data
@@ -31,79 +32,73 @@ public class TransferFundsTest extends BaseTestFunctionality {
         //Login
         login(overviewPage, configLoader, driver);
 
-        //Check the page title
-        System.out.println(getPageTitle());
-        Assert.assertTrue(getPageTitle().equalsIgnoreCase("ParaBank | Accounts Overview"), "Overview page not loaded");
-
-
-        //Check if the number of accounts
-        if(overviewPage.getNoOfAccounts() == 1) {
-            //Get account ID
-            String account = overviewPage.getAccountList().getFirst();
-
-            //Go to Open New Account
-            overviewPage.clickOpenNewAccountLink();
-
-            //Check page title
-            System.out.println(getPageTitle());
-            Assert.assertTrue(getPageTitle().equalsIgnoreCase("ParaBank | Open Account"), "Overview page not loaded");
-
-
-            //Set account type
-            openNewAccountPage.selectAccountType("CHECKING");
-
-            //Set account
-            openNewAccountPage.selectAccount(account);
-
-            //Check selected options
-            System.out.println("Number of 'Account Type' selected is " + openNewAccountPage.getSelectedAccountType().size());
-            Assert.assertEquals(openNewAccountPage.getSelectedAccountType().getFirst(), "CHECKING", "");
-            System.out.println("Number of 'Account' selected is " + openNewAccountPage.getSelectedAccount().size());
-            Assert.assertEquals(openNewAccountPage.getSelectedAccount().getFirst(), account, "");
-
-            //Click OPEN NEW ACCOUNT
-            openNewAccountPage.clickOpenNewAccountButton();
-
-            //Check result message
-            Assert.assertTrue(openNewAccountPage.getResultMessage().
-                    equalsIgnoreCase("Account Opened!"), "Account not created");
-
-            //Get new account number
-            String newAccount = openNewAccountPage.getNewAccountNumber();
-
-            //Go to overview page
-            openNewAccountPage.clickAccountsOverviewLink();
-
-            //Check if the new account is in the list
-            Assert.assertTrue(overviewPage.checkAccount(newAccount), "Account not found");
-
-        }
+        //Open new account
+        String newAccount = getNewAccount(overviewPage, openNewAccountPage, driver);
 
 
         //Get the accounts balance
         List<String> accounts = overviewPage.getAccountList();
         List<String> balances = overviewPage.getBalanceList().stream().map(e -> e.replace("$","")).toList();
-        double expectedTotalValue = Double.parseDouble(balances.getFirst()) + Double.parseDouble(balances.get(1));
+
+        String newAccountBalance = balances.get(accounts.indexOf(newAccount));
+        double expectedTotalValue = Double.parseDouble(balances.getFirst()) + Double.parseDouble(newAccountBalance);
+
+
+        //Declare transfer amount
+        String transferAmount = "111";
+
+        //Transfer funds
+        transferFunds(overviewPage, driver, transferAmount, accounts.getFirst(), newAccount);
+
+
+        //Check if the transaction was successful
+        List<String> currentBalances = overviewPage.getBalanceList().stream().map(e -> e.replace("$","")).toList();
+        String newCurrentAccountBalance = currentBalances.get(accounts.indexOf(newAccount));
+        double currentTotalValue = Double.parseDouble(currentBalances.getFirst()) + Double.parseDouble(newCurrentAccountBalance);
+
+        Assert.assertEquals(currentTotalValue, expectedTotalValue,"Sums don't match");
+        Assert.assertEquals(Double.parseDouble(balances.getFirst()) - Double.parseDouble(transferAmount),
+                Double.parseDouble(currentBalances.getFirst()),
+                "wrong amount extracted from account");
+        Assert.assertEquals(Double.parseDouble(newAccountBalance) + Double.parseDouble(transferAmount),
+                Double.parseDouble(newCurrentAccountBalance),
+                "wrong amount extracted from account");
+
+
+    }
+
+
+    /**
+     * Function used to transfer founds from fromAccount to toAccount
+     * @param overviewPage Used to interact with the overview page
+     * @param driver Driver passed throughout the project
+     * @param transferAmount Amount of money to be transferred
+     * @param fromAccount Indicates the sending account
+     * @param toAccount Indicates the receiving account
+     */
+    public static void transferFunds(OverviewPageActions overviewPage, WebDriver driver, String transferAmount, String fromAccount, String toAccount) {
+
+        //Make initialization
+        TransferFundsPageActions transferFundsPage = new TransferFundsPageActions(driver);
 
         //Navigate to Transfer Founds
         overviewPage.clickTransferFundsLink();
 
         //Check the page title
-        System.out.println(getPageTitle());
-        Assert.assertTrue(getPageTitle().equalsIgnoreCase("ParaBank | Transfer Funds"), "Transfer Funds page not loaded");
+        System.out.println(getPageTitle(driver));
+        Assert.assertTrue(getPageTitle(driver).equalsIgnoreCase("ParaBank | Transfer Funds"), "Transfer Funds page not loaded");
 
 
         //Fill in the transaction details
-        String transferAmount = "100";
         transferFundsPage.typeAmount(transferAmount);
-        transferFundsPage.selectFromAccount(accounts.getFirst());
-        transferFundsPage.selectToAccount(2);
+        transferFundsPage.selectFromAccount(fromAccount);
+        transferFundsPage.selectToAccount(toAccount);
 
         //Check if the selected options are right
         Assert.assertEquals(transferFundsPage.getSelectedFromAccount().size(), 1);
         Assert.assertEquals(transferFundsPage.getSelectedToAccount().size(), 1);
-        Assert.assertEquals(accounts.getFirst(), transferFundsPage.getSelectedFromAccount().getFirst());
-        Assert.assertEquals(accounts.get(1), transferFundsPage.getSelectedToAccount().getFirst());
+        Assert.assertEquals(fromAccount, transferFundsPage.getSelectedFromAccount().getFirst());
+        Assert.assertEquals(toAccount, transferFundsPage.getSelectedToAccount().getFirst());
 
         //Press Transfer
         transferFundsPage.clickTransferButton();
@@ -116,27 +111,8 @@ public class TransferFundsTest extends BaseTestFunctionality {
         transferFundsPage.clickAccountsOverviewLink();
 
         //Check to page title
-        System.out.println(getPageTitle());
-        Assert.assertTrue(getPageTitle().equalsIgnoreCase("ParaBank | Accounts Overview"), "Overview page not loaded");
-
-
-        //Check if the transaction was successful
-        List<String> currentBalances = overviewPage.getBalanceList().stream().map(e -> e.replace("$","")).toList();
-        double currentTotalValue = Double.parseDouble(currentBalances.getFirst()) + Double.parseDouble(currentBalances.get(1));
-
-        Assert.assertEquals(currentTotalValue, expectedTotalValue,"Sums don't match");
-        Assert.assertEquals(Double.parseDouble(balances.getFirst()) - Double.parseDouble(transferAmount),
-                Double.parseDouble(currentBalances.getFirst()),
-                "wrong amount extracted from account");
-        Assert.assertEquals(Double.parseDouble(balances.get(1)) + Double.parseDouble(transferAmount),
-                Double.parseDouble(currentBalances.get(1)),
-                "wrong amount extracted from account");
-
-
-
-        System.out.println(transferFundsPage.getPageHeaderText());
-        System.out.println(getPageTitle());
-
+        System.out.println(getPageTitle(driver));
+        Assert.assertTrue(getPageTitle(driver).equalsIgnoreCase("ParaBank | Accounts Overview"), "Overview page not loaded");
 
     }
 

f31a806791773e3ec7aa7caede54fdb773127773
MarinAdrian <adymarin013@gmail.com>
2024-07-16 11:38:54 +0300

    create a new findCustomer function

--- homeAssignment/test/java/tests/ForgotLoginInfoTest.java
+++ homeAssignment/test/java/tests/ForgotLoginInfoTest.java
@@ -2,10 +2,15 @@ package tests;
 
 import actions.CustomerLookupPageActions;
 import actions.HomePageActions;
+import actions.OverviewPageActions;
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import utile.ConfigLoader;
 import utilities.BaseTestFunctionality;
+import utilities.ConfigurationLoader;
+
+import static tests.LoginTest.login;
+import static tests.RegisterTest.signUpActions;
 
 public class ForgotLoginInfoTest extends BaseTestFunctionality {
 
@@ -19,16 +24,60 @@ public class ForgotLoginInfoTest extends BaseTestFunctionality {
         HomePageActions homePage = new HomePageActions(driver);
         CustomerLookupPageActions customerLookupPage = new CustomerLookupPageActions(driver);
 
+        //Get registration data
+        ConfigurationLoader configLoader = new ConfigurationLoader("homeAssignment/test/resources/properties/CornelMoroi.properties");
+
+        findCustomer(overviewPage, driver, configLoader);
+    }
+
+    @Test
+    public void forgotLoginInfo2(){
+
+        //Create test
+        initTest("Forgot Login Info Test2");
+
+        //Make initialization
+        HomePageActions homePage = new HomePageActions(driver);
+        CustomerLookupPageActions customerLookupPage = new CustomerLookupPageActions(driver);
+
+        //Get registration data
+        ConfigurationLoader configLoader = new ConfigurationLoader("homeAssignment/test/resources/properties/MirceaGrad.properties");
+
+        findCustomer(overviewPage, driver, configLoader);
+    }
+
+    public static void findCustomer(OverviewPageActions overviewPage, WebDriver driver, ConfigurationLoader configLoader) {
+
+        //Make initializations
+        HomePageActions homePage = new HomePageActions(driver);
+        CustomerLookupPageActions customerLookupPage = new CustomerLookupPageActions(driver);
+
+        //Ensure that account exists
+        try{
+            //SignUp
+            signUpActions(configLoader,driver);
+        } catch (Exception e){
+            //Login
+            login(overviewPage, configLoader, driver);
+        }
+
+        //Log Out
+        overviewPage.clickLogOutLink();
+
+        //Check if log out was successful
+        String expectedText = "atm services";
+        String ATMServicesText = homePage.getATMServicesText();
+
+        Assert.assertEquals(ATMServicesText.toLowerCase(), expectedText.toLowerCase());
+
+
         //Navigate to Forgot Login Info page
         homePage.clickForgotLoginInfoLink();
 
         //Check the page title
-        System.out.println(getPageTitle());
-        Assert.assertTrue(getPageTitle().equalsIgnoreCase("ParaBank | Customer Lookup"), "Customer Lookup page not loaded");
+        System.out.println(getPageTitle(driver));
+        Assert.assertTrue(getPageTitle(driver).equalsIgnoreCase("ParaBank | Customer Lookup"), "Customer Lookup page not loaded");
 
-
-        //Get registration data
-        ConfigLoader configLoader = new ConfigLoader("homeAssignment/test/resources/properties/MirceaGrad.properties");
 
         customerLookupPage.enterFirstName(configLoader.getProperty("firstName"));
         customerLookupPage.enterLastName(configLoader.getProperty("lastName"));

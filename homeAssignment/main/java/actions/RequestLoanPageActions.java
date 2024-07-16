d32e30e9b9adf3d48347253065a96a7d7355868b
MarinAdrian <adymarin013@gmail.com>
2024-07-17 01:21:18 +0300

    request loan page interactions implemented in code

--- homeAssignment/main/java/actions/RequestLoanPageActions.java
+++ homeAssignment/main/java/actions/RequestLoanPageActions.java
@@ -12,5 +12,27 @@ public class RequestLoanPageActions extends MenuLinksActions {
         this.elements = new RequestLoanPageElements(driver);
     }
 
+    public void enterLoanAmount(String loanAmount){
+        elements.loanAmount().sendKeys(loanAmount);
+    }
+
+    public void enterDownPayment(String downPayment){
+        elements.downPayment().sendKeys(downPayment);
+    }
+
+    public void selectFromAccount(String fromAccount){
+        elements.fromAccount().selectByValue(fromAccount);
+    }
+
+    public void clickApplyNowButton(){
+        elements.applyNowButton().click();
+    }
+
+    public String getRequestResultMessage(){
+        wait.until(_ -> elements.getResultTable().isDisplayed());
+        wait.until(_ -> !elements.requestResultMessage().getText().isEmpty());
+        return elements.requestResultMessage().getText();
+    }
+
 
 }
